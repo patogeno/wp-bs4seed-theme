e65523059667a3da69d16ac2c0109c5715c81794
Patricio Soto Kort <psotokort@gmail.com>
2020-08-30 00:06:50 +0800

    Adds 404 template

---
+++ bs4seed/404.php
@@ -0,0 +1,11 @@
+<?php
+/* 404 Page */
+    get_header();
+?>
+
+<main class="container bs4seed-main error-404">
+    <h1>Error 404</h1>
+    <p>Page Not Found</p>
+</main>
+
+<?php get_footer(); ?> 
