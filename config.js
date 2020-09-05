426fb0359994cfe80faf08e9a3a91ce0cb187a03
Patricio Soto Kort <psotokort@gmail.com>
2020-09-06 00:39:22 +0800

    Changes config.json to dotenv for configuration

--- config.js
+++ config.js
@@ -1,74 +1,84 @@
-const rls = require("readline-sync");
-const fs = require("fs");
+const rls = require("readline-sync")
+const fs = require("fs")
+const os = require("os")
 
-const DEFAULT_THEME_SLUG = "bs4seed";
-const DEFAULT_THEME_SERVER_FOLDER = "C:/wamp64/www/wordpress/wp-content/themes/";
-const CONFIG_FILE = "config.json";
+const DEFAULT_THEME_SLUG = "bs4seed"
+const DEFAULT_THEME_SERVER_FOLDER = "C:/wamp64/www/wordpress/wp-content/themes/"
+const ENV_FILE = ".env"
 
-/* FIRST QUESTION: THEME SLUG */
+/** FIRST QUESTION: THEME SLUG **/
 const maxRetries = 5;
 let themeSlug = null;
+
+// Loop to repeat question until slug is correct or maximum retries is reached
 for (let i=0; i<maxRetries; i++) {
     if (i==0) {
-        themeSlug = rls.question("The theme slug is used as folder name for your theme and general reference.\n\
-It must only contain letters (lowercase), numbers and dashes (no special characters or spaces).\n\
-What is the theme slug? (" + DEFAULT_THEME_SLUG + ") ");
+        themeSlug = rls.question(`The theme slug is used as folder name for your theme and general reference.${os.EOL}\
+It must only contain letters (lowercase), numbers and dashes (no special characters or spaces).${os.EOL}\
+What is the theme slug? (${DEFAULT_THEME_SLUG}) `)
     } else {
-        themeSlug = rls.question("Slug is not valid. Please try again: ");
+        themeSlug = rls.question("Slug is not valid. Please try again: ")
     }
-    themeSlug = themeSlug.trim();
+    // Trim answer
+    themeSlug = themeSlug.trim()
 
-    // Default Slug
+    // Default Slug if there is no answer
     if(themeSlug.length == 0)  {
-        themeSlug = DEFAULT_THEME_SLUG;
-        break;
+        themeSlug = DEFAULT_THEME_SLUG
+        break
     }
     // Validate Slug
     else if(/^[a-z0-9]+(?:-[a-z0-9]+)*$/.test(themeSlug)) {
-        break;
+        break
     } else {
-        themeSlug = null;
+        themeSlug = null
     }
 }
+// Check if maximum retries were reached or slug was correctly defined.
 if (themeSlug == null) {
-    console.log("You have reached the maximum number of retries to choose a slug. Good bye!");
+    console.log("You have reached the maximum number of retries to choose a slug. Good bye!")
+    return
 } else {
-    console.log("Theme Slug: " + themeSlug);
+    console.log(`Theme Slug: ${themeSlug}`)
 }
 
-/* SECOND QUESTION: THEME SERVER FOLDER */
-let themeServerFolder = rls.question("What is the folder where your compiled theme will be copied to? (" + DEFAULT_THEME_SERVER_FOLDER + ") ");
-themeServerFolder = themeServerFolder.trim();
+/** SECOND QUESTION: THEME SERVER FOLDER **/
+let themeServerFolder = rls.question(`What is the folder where your compiled theme will be copied to? (${DEFAULT_THEME_SERVER_FOLDER}) `)
+// Trim answer
+themeServerFolder = themeServerFolder.trim()
 
-// Default Theme Server Folder
+// Default Theme Server Folder if there is no answer
 if (themeServerFolder.length == 0) {
-    themeServerFolder = DEFAULT_THEME_SERVER_FOLDER;
+    themeServerFolder = DEFAULT_THEME_SERVER_FOLDER
 }
-console.log("Theme Server Folder: " + themeServerFolder);
+console.log(`Theme Server Folder: ${themeServerFolder}`)
 
-/* WRITE CONFIG FILE */
+/** WRITE CONFIG FILE **/
 let previousThemeSlug;
-fs.readFile(CONFIG_FILE, 'utf8', function (err,data) {
+fs.readFile(ENV_FILE, 'utf8', function (err,data) {
+    // if file does not exist, create file with selected parameters
     if (err) {
-        return console.log(err);
+        const textToWrite = `THEME_SLUG=${themeSlug}${os.EOL}THEME_SERVER_FOLDER="${themeServerFolder}"${os.EOL}`
+        // Write new file
+        fs.writeFile(ENV_FILE, textToWrite, 'utf8', function (err) {if (err) throw err});
+        previousThemeSlug = DEFAULT_THEME_SLUG
+        console.log(`File ${ENV_FILE} has been created.`)
     }
+    // If file exists, update configuration
+    else {
+        previousThemeSlug = data.match(/THEME_SLUG\s*=\s*(.*)/)[1]
+        let result = data.replace(/THEME_SLUG\s*=\s*.*/g, `THEME_SLUG=${themeSlug}`)
+        result = result.replace(/THEME_SERVER_FOLDER\s*=\s*".*"/g, `THEME_SERVER_FOLDER="${themeServerFolder}"`)
+    
+        // Update file
+        fs.writeFile(ENV_FILE, result, 'utf8', function (err) {if (err) throw err})
 
-    previousThemeSlug = data.match(/"themeSlug"\s*:\s*"(.*)"/)[1];
-    let result = data.replace(/"themeSlug"\s*:\s*".*"/g, '"themeSlug" : "' + themeSlug + '"');
-    result = result.replace(/"themeServerFolder"\s*:\s*".*"/g, '"themeServerFolder" : "' + themeServerFolder + '"');
-  
-    fs.writeFile(CONFIG_FILE, result, 'utf8', function (err) {
-       if (err) return console.log(err);
-    });
-
-    console.log(`File ${CONFIG_FILE} has been updated.`);
-
+        console.log(`File ${ENV_FILE} has been updated.`)
+    }
+    
     /* RENAME THEME FOLDER */
-    fs.rename("./" + previousThemeSlug, "./" + themeSlug, function(err) {
-        if (err) {
-        console.log(err);
-        } else {
-        console.log("Theme Folder was successfully renamed as per selected slug.");
-        }
+    fs.rename(`./${previousThemeSlug}`, `./${themeSlug}`, function(err) {
+        if (err) throw err
+        else console.log("Theme Folder was successfully renamed as per selected slug.")
     })            
-});
+})
--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,49 @@
-const { watch, src, dest, series, task } = require('gulp');
-const uglify = require('gulp-uglifyjs');
-const sass = require('gulp-sass');
-const wait = require('gulp-wait');
-const replace = require('gulp-replace');
-const changed = require('gulp-changed');
-const autoprefixer = require('gulp-autoprefixer');
-const sourcemaps = require('gulp-sourcemaps');
-const merge = require('merge-stream');
-const imagemin = require('gulp-imagemin');
-const pngquant = require('imagemin-pngquant');
-const jpegtran = require('imagemin-jpegtran');
-const config = require('./config.json');
+const { watch, src, dest, series, task } = require('gulp')
+const uglify = require('gulp-uglifyjs')
+const sass = require('gulp-sass')
+const wait = require('gulp-wait')
+const replace = require('gulp-replace')
+const changed = require('gulp-changed')
+const autoprefixer = require('gulp-autoprefixer')
+const sourcemaps = require('gulp-sourcemaps')
+const merge = require('merge-stream')
+const imagemin = require('gulp-imagemin')
+const pngquant = require('imagemin-pngquant')
+const jpegtran = require('imagemin-jpegtran')
+require('dotenv').config()
 
-const THEME_SLUG = config.general.themeSlug;
-const THEME_FOLDER_NAME = './' + THEME_SLUG;
-const THEME_SERVER_FOLDER = config.general.themeServerFolder + THEME_SLUG;
-
-console.log(THEME_SLUG, THEME_SERVER_FOLDER);
+if ( process.env.THEME_SLUG == null ) {
+    console.error("ERROR: Theme Slug not defined.\nRun `npm run config` to create .env with enviroment variables required for Gulp")
+    return
+}
+const THEME_SLUG = process.env.THEME_SLUG
+const THEME_FOLDER_NAME = './' + THEME_SLUG
+const THEME_SERVER_FOLDER = process.env.THEME_SERVER_FOLDER + THEME_SLUG
 
 function imageminTask(cb) {
-    const SOURCE = THEME_FOLDER_NAME + '/assets/images/**/*';
-    const DESTINATION = THEME_SERVER_FOLDER + '/assets/images';
+    const SOURCE = THEME_FOLDER_NAME + '/assets/images/**/*'
+    const DESTINATION = THEME_SERVER_FOLDER + '/assets/images'
     return src(SOURCE)
         .pipe(imagemin({
             progressive: true,
             svgoPlugins: [{removeViewBox: false}],
             use: [pngquant(),jpegtran()]
         }))
-        .pipe(dest(DESTINATION));
-    cb();
+        .pipe(dest(DESTINATION))
+    cb()
 }
 
 function imageOptimiser(cb) {
-    const SOURCE = './image-optimiser/in/**/*';
-    const DESTINATION = './image-optimiser/out/';
+    const SOURCE = './image-optimiser/in/**/*'
+    const DESTINATION = './image-optimiser/out/'
     return src(SOURCE)
         .pipe(imagemin({
             progressive: true,
             svgoPlugins: [{removeViewBox: false}],
             use: [pngquant(),jpegtran()]
         }))
-        .pipe(dest(DESTINATION));
-    cb();
+        .pipe(dest(DESTINATION))
+    cb()
 }
 
 function sassTask(cb) {
@@ -51,8 +53,8 @@ function sassTask(cb) {
             .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
             .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
         .pipe(sourcemaps.write('./'))
-        .pipe(dest(THEME_SERVER_FOLDER + '/assets/css'));
-    cb();
+        .pipe(dest(THEME_SERVER_FOLDER + '/assets/css'))
+    cb()
 }
 
 function mainVersionUpdate(cb) {
@@ -63,49 +65,49 @@ function mainVersionUpdate(cb) {
                 return "cacheNumber = '" + cbString;
             })
         )
-        .pipe(dest(THEME_FOLDER_NAME + '/'));
-    cb();
+        .pipe(dest(THEME_FOLDER_NAME + '/'))
+    cb()
 }
 
 function uglifyTask(cb) {
     return src(THEME_FOLDER_NAME + '/assets/js/*.js')
         .pipe(uglify(THEME_SLUG + '.min.js'))
-        .pipe(dest(THEME_SERVER_FOLDER + '/assets/js'));
-    cb();
+        .pipe(dest(THEME_SERVER_FOLDER + '/assets/js'))
+    cb()
 }
 
 function rootFiles(cb) {
-    const SOURCE = THEME_FOLDER_NAME + '/*.{css,php,png}';
+    const SOURCE = THEME_FOLDER_NAME + '/*.{css,php,png}'
     const DESTINATION = THEME_SERVER_FOLDER;
     return src(SOURCE)
         .pipe(changed(DESTINATION))
-        .pipe(dest(DESTINATION));
-    cb();
+        .pipe(dest(DESTINATION))
+    cb()
 }
 
-var additional_assets = ['php','fonts'];
-function additionalAssets(cb) {
-    var tasks = additional_assets.map(function(folder){
+const additionalAssets = ['php','fonts'];
+function additionalAssetsTask(cb) {
+    const tasks = additionalAssets.map(function(folder) {
         const SOURCE = THEME_FOLDER_NAME + '/assets/' + folder + '/**/*';
         const DESTINATION = THEME_SERVER_FOLDER + '/assets/' + folder;
         return src(SOURCE)
             .pipe(changed(DESTINATION))
-            .pipe(dest(DESTINATION));
-    });
+            .pipe(dest(DESTINATION))
+    })
     return merge(tasks);
-    cb();
+    cb()
 }
 
 exports.default = function() {
-    watch(THEME_FOLDER_NAME + '/assets/sass/**/*.scss', series(sassTask, mainVersionUpdate));
-    watch(THEME_FOLDER_NAME + '/assets/js/**/*.js', uglifyTask);
-    watch(THEME_FOLDER_NAME + '/*.{css,php,png}', rootFiles);
-    watch(THEME_FOLDER_NAME + '/assets/images/**/*', imageminTask);
-    watch(THEME_FOLDER_NAME + '/assets/php/*', additionalAssets);
+    watch(THEME_FOLDER_NAME + '/assets/sass/**/*.scss', series(sassTask, mainVersionUpdate))
+    watch(THEME_FOLDER_NAME + '/assets/js/**/*.js', uglifyTask)
+    watch(THEME_FOLDER_NAME + '/*.{css,php,png}', rootFiles)
+    watch(THEME_FOLDER_NAME + '/assets/images/**/*', imageminTask)
+    watch(THEME_FOLDER_NAME + '/assets/php/**/*', additionalAssetsTask)
 };
 
-exports.images = imageminTask;
-exports.mainVersionUpdate = mainVersionUpdate;
-exports.imageOptimiser = imageOptimiser;
-exports.build = series(rootFiles, sassTask, mainVersionUpdate, uglifyTask, imageminTask, additionalAssets);
-exports.sass = series(sassTask, mainVersionUpdate);
+exports.images = imageminTask
+exports.mainVersionUpdate = mainVersionUpdate
+exports.imageOptimiser = imageOptimiser
+exports.build = series(rootFiles, sassTask, mainVersionUpdate, uglifyTask, imageminTask, additionalAssetsTask)
+exports.sass = series(sassTask, mainVersionUpdate)
