75049dc845da3c46a872ea0c231ff7fe730cfeae
Patricio Soto Kort <psotokort@gmail.com>
2020-09-06 01:15:16 +0800

    Adds sidebar to index template

--- bs4seed/index.php
+++ bs4seed/index.php
@@ -3,15 +3,21 @@
     get_header();
 ?>
 
-<main class="container bs4seed-main">
-    <?php 
-        if (have_posts()) :
-            while(have_posts()) :
-                the_post();
-                    the_content();
-            endwhile;
-        endif;
-    ?>
-</main>
-
+<div class="content-wrapper container">
+    <div class="row">
+        <main class="bs4seed-main col-12 col-md-8">
+            <?php 
+                if (have_posts()) :
+                    while(have_posts()) :
+                        the_post();
+                            the_content();
+                    endwhile;
+                endif;
+            ?>
+        </main>
+        <aside class="bs4seed-aside .d-none col-md-4">
+            <?php get_sidebar(); ?> 
+        </aside>
+    </div>
+</div>
 <?php get_footer(); ?> 
